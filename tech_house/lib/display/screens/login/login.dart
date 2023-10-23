1f8e16ac127daf617c91fb30ef9b90dd035e524c
Mane-jaker <angelillo3581@gmail.com>
2023-10-23 00:04:31 -0600

    hola login

---
+++ tech_house/lib/display/screens/login/login.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            const Text(
+              'Ingresar',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+                'Ingresa a la aplicación con tu correo o mediante Gmail'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Iniciar sesión con Google
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+              ),
+              child: const Text('Iniciar sesión con Google'),
+            ),
+            const SizedBox(height: 20),
+            const Divider(
+              height: 20,
+              thickness: 2,
+              endIndent: 30,
+              indent: 30,
+            ),
+            const SizedBox(height: 20),
+            const TextField(
+              decoration: InputDecoration(
+                hintText: 'Ingresa tu correo',
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('No tienes cuenta '),
+                TextButton(
+                  onPressed: () {
+                    // Navegar a la página de registro
+                  },
+                  child: const Text(
+                    'REGISTRATE',
+                    style: TextStyle(
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Acceder
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.lightBlue,
+              ),
+              child: const Text('Acceder'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- tech_house/lib/main.dart
+++ tech_house/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tech_house/display/widgets/global/navbar_bottom.dart';
+import 'package:tech_house/display/screens/login/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TechHouse',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const NavbarBottom(title: "TechHouse"),
+      debugShowCheckedModeBanner: false,
+      home: const Login(),
     );
   }
 }
