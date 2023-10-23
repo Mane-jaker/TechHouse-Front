4d5166f18ae5e7b77db720ae239bd55211320e67
Mane-jaker <angelillo3581@gmail.com>
2023-10-22 21:03:49 -0600

    siiiiiiiii

--- tech_house/lib/main.dart
+++ tech_house/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:tech_house/display/widgets/global/navbar_bottom.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +14,6 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const NavbarBottom(title: "TechHouse"),
     );
   }
 }
