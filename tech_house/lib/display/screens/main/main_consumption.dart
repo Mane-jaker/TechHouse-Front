24062018203de8b60e044d91bebf237a5d6d2e14
Mane-jaker <angelillo3581@gmail.com>
2023-10-22 20:53:50 -0600

    hola proyecto

--- tech_house/lib/display/screens/main/main_consumption.dart
+++ tech_house/lib/display/screens/main/main_consumption.dart
@@ -6,7 +6,7 @@ class MainConsuptiom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Text("hola"),
+      child: Text("puta"),
     );
   }
 }
--- tech_house/lib/main.dart
+++ tech_house/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tech_house/display/widgets/global/navbar_bottom.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: const NavbarBottom(title: "TechHouse"),
     );
   }
 }
