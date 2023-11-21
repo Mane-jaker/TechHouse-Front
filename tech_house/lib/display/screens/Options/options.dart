e1c4273a1668d3a08f0a4900a8d30008088c0b5e
Esteban <98122124+EpsyUwU@users.noreply.github.com>
2023-11-21 00:15:14 -0600

    Pagina de opciones, falta ver que va en los desplegables

--- tech_house/lib/display/screens/Options/options.dart
+++ tech_house/lib/display/screens/Options/options.dart
@@ -1,12 +1,82 @@
 import 'package:flutter/material.dart';
 
-class Options extends StatelessWidget {
+class Options extends StatefulWidget {
   const Options({super.key});
 
   @override
+  _OptionsState createState() => _OptionsState();
+}
+
+class _OptionsState extends State<Options> {
+  int _currentIndex = -1;
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Estos seran los options xd"),
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          CircleAvatar(
+            backgroundColor: Colors.blue,
+            child: Icon(Icons.person, color: Colors.white, size: 55.0),
+            radius: 55.0,
+          ),
+          SizedBox(height: 25,),
+          Text(
+            'Esteban Rivera',
+            style: TextStyle(fontSize: 24.0),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 50,),
+          ExpansionPanelList(
+            expansionCallback: (int index, bool isExpanded) {
+              setState(() {
+                _currentIndex = _currentIndex == index ? -1 : index;
+              });
+            },
+            children: [
+              ExpansionPanel(
+                headerBuilder: (BuildContext context, bool isExpanded) {
+                  return ListTile(
+                    title: Text('Datos de la cuenta'),
+                  );
+                },
+                body: Column(
+                  children: [
+                    // Aquí puedes agregar el contenido que se mostrará cuando se expanda 'Datos de la cuenta'
+                  ],
+                ),
+                isExpanded: _currentIndex == 0,
+              ),
+              ExpansionPanel(
+                headerBuilder: (BuildContext context, bool isExpanded) {
+                  return ListTile(
+                    title: Text('Notificaciones'),
+                  );
+                },
+                body: Column(
+                  children: [
+                    // Aquí puedes agregar el contenido que se mostrará cuando se expanda 'Notificaciones'
+                  ],
+                ),
+                isExpanded: _currentIndex == 1,
+              ),
+              ExpansionPanel(
+                headerBuilder: (BuildContext context, bool isExpanded) {
+                  return ListTile(
+                    title: Text('Sensores'),
+                  );
+                },
+                body: Column(
+                  children: [
+                    // Aquí puedes agregar el contenido que se mostrará cuando se expanda 'Sensores'
+                  ],
+                ),
+                isExpanded: _currentIndex == 2,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
